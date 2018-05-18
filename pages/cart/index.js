5afc4eb65f0ea0072cc2eae5a988f7618604b336
caylal <871367258@qq.com>
2018-05-18 14:30:34 +0800

    加了一点东西

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -4,7 +4,8 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '购物车',   
+    cart: [
+      { title: "商品上门", titleColor: "title-red", store: [{ avatar: "../../images/pic.jpg", name: "邻近生活", goods: [{ pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "25.5", num: "3" }, { pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "21", num: "1" }] }, { avatar: "../../images/pic.jpg", name: "邻近生活", goods: [{ pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "25.5", num: "3" }, { pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "21", num: "1" }] }] }, { title: "洗护上门", titleColor: "title-green", store: [{ avatar: "../../images/pic.jpg", name: "邻近生活", goods: [{ pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "25.5", num: "3" }, { pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "21", num: "1" }] }, { avatar: "../../images/pic.jpg", name: "邻近生活", goods: [{ pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "25.5", num: "3" }, { pic: "../../images/orange.png", title: "华润五丰 美仑达三峡秭归伦晚脐橙 2.5kg装 精品果", price: "21", num: "1" }] }] }],   
   }, 
   onLoad: function () {    
     
--- pages/orders/index.js
+++ pages/orders/index.js
@@ -1,12 +1,33 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+let goods = [{ type: "商品订单", status: "未发货", no: "15486126489615265", time: "5-17 18:01" }]
+let wash = [{type: "洗护订单", status: "未发货", no: "15486126489615265", time: "5-17 18:01" }]
+let all = goods.concat(wash)
 Page({
   data: {
-    motto: '订单',
+    tab: [{ type: "all", title: "全部" }, { type: "goods", title: "商品订单" },{ type: "wash", title: "洗护订单" }],
+    orderList: [],
+    currentid: 0,
   },
-  onLoad: function () {
-
+  onLoad() {
+    this.setData({
+      orderList: all
+    })
+  },
+  bindTap(e) {
+    let id = e.currentTarget.dataset.type;
+    let list = [];
+    if( id == 'goods') {
+      list = goods;
+    } else if( id == 'wash'){
+      list = wash;
+    }else{
+      list = all;
+    }
+    this.setData({
+      currentid: e.currentTarget.dataset.id,
+      orderList: list
+    })
   }
 })
